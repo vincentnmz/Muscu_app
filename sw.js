431b3708006056c625720b5cf373cf2bda092453
vincentnmz <portefaixvincent@gmail.com>
2026-08-13 10:50:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * jamais mis en cache : ils passent au réseau, et l'app gère le hors-ligne
  * (file d'attente des séances) de son côté.
  */
-const CACHE = 'novalyz-shell-v30';
+const CACHE = 'novalyz-shell-v32';
 const ASSETS = [
   './',
   './index.html',
